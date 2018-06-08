a1bc108e2dacd26d73673bf302f72980a3ce052a
samuellup <slup@umh.es>
2018-06-08 11:57:12 +0200

    -2

--- an_scripts/cr-analysis.py
+++ an_scripts/cr-analysis.py
@@ -59,3 +59,96 @@ with open(args.out, "a") as meta:
 
 
 #print args.rd, args.mps, n_candidates, cr_span
+
+
+
+########################################## Merged analysis ######################################################################################################
+'''
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-in_va', action="store", dest='in_va', required=True)
+parser.add_argument('-out', action="store", dest='out', required=True)
+parser.add_argument('-mps', action="store", dest='mps', required=True)
+parser.add_argument('-rd', action="store", dest='rd', required=True)
+args = parser.parse_args()
+
+candidates_98=list()
+candidates_95=list()
+
+#Default values
+n_candidates_95=0
+n_candidates_98=0
+
+cr_span_95=0
+cr_span_98=0
+
+with open(args.in_va, "r") as variants:
+	for line in variants:
+		if not line.startswith("#"):
+			sp = line.split()
+			position = int(sp[1])
+			af = float(sp[6])/(float(sp[6]) + float(sp[5]))
+			if af > 0.95:
+				coord=[position, af]
+				candidates_95.append(coord)
+
+with open(args.in_va, "r") as variants:
+	for line in variants:
+		if not line.startswith("#"):
+			sp = line.split()
+			position = int(sp[1])
+			af = float(sp[6])/(float(sp[6]) + float(sp[5]))
+			if af > 0.98:
+				coord=[position, af]
+				candidates_98.append(coord)
+
+
+pos_min_95=float("inf")
+pos_max_95=0
+
+pos_min_98=float("inf")
+pos_max_98=0
+
+
+# With curation
+for var in candidates_95:
+	try:
+		d_95 = var[0] - ant_95
+		if d_95 < 4000000:
+			if var[0] < pos_min_95: pos_min_95 = var[0]
+			if var[0] > pos_max_95: pos_max_95 = var[0]
+			cr_span_95 = pos_max_95 - pos_min_95
+			if cr_span_95 == 0: cr_span_95 = 1
+		ant_95 = var[0]
+
+	except:
+		ant_95 = var[0]
+
+
+for var in candidates_98:
+	try:
+		d_98 = var[0] - ant_98 
+		if d_98 < 4000000:
+			if var[0] < pos_min_98: pos_min_98 = var[0]
+			if var[0] > pos_max_98: pos_max_98 = var[0]
+			cr_span_98 = pos_max_98 - pos_min_98
+			if cr_span_98 == 0: cr_span_98 = 1
+		ant_98 = var[0]
+
+	except:
+		ant_98 = var[0]
+
+
+
+
+n_candidates_95 = len(candidates_95)
+n_candidates_98 = len(candidates_98)
+
+with open(args.out, "a") as meta:
+	meta.write(str(args.rd) + "\t" + str(args.mps) + "\t" + str(n_candidates_95) + "\t" + str(cr_span_95) + str(n_candidates_98) + "\t" + str(cr_span_98) + "\n")
+
+
+'''
+
+########################################## Merged analysis ######################################################################################################
